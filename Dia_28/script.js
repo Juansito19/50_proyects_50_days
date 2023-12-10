6c0e7aa3c5f36461a5e600093f824034ca76c37f
Juansito19 <juanbro68@gmail.com>
2023-12-10 19:46:27 +0100

    3 proyectos

---
+++ Dia_28/script.js
@@ -0,0 +1,102 @@
+const APIURL = 'https://api.github.com/users/'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
+
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username)
+
+    createUserCard(data)
+    getRepos(username)
+  } catch(error) {
+    if(error.response.status == 404) {
+      createErrorCard('No existe un usuario con ese nombre❌')
+    }
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + '/repos?sort=pushed')
+
+    addReposToCard(data)
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById('repos')
+
+  repos
+    .slice(0, 10)
+    .forEach(repo => {
+      const repoEl = document.createElement('a')
+      repoEl.classList.add('repo')
+      repoEl.href = repo.html_url
+      repoEl.target = '_blank'
+      repoEl.innerText = repo.name
+
+      reposEl.appendChild(repoEl)
+  });
+}
+
+function createUserCard(user) {
+  let bio = user.bio
+  if(bio === null) {
+    bio = ''
+  } else {
+    bio = user.bio
+  }
+
+  const cardHTML = `
+  <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${user.name}</h2>
+      <p>${bio}</p>
+
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+
+      <div id="repos"></div>
+    </div>
+  </div>
+`
+
+  main.innerHTML = cardHTML
+
+
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+  <div class="card">
+    <h1>${msg}</h1>
+  </div>
+  ` 
+
+  main.innerHTML = cardHTML
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const user = search.value
+
+  if(user) {
+    getUser(user)
+
+    search.value = ''
+  }
+})
+
+
+
